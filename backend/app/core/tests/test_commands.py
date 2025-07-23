6893992773dc1d86491c907d79562409af3cb5a6
Mateusz Dalke <sculpttechproject@gmail.com>
2025-07-23 19:47:56 +0200

    created tests for wait_for_db

--- backend/app/core/tests/test_commands.py
+++ backend/app/core/tests/test_commands.py
@@ -9,3 +9,27 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+
+@patch("core.management.commands.wait_for_db.Command.check")
+class command_test(SimpleTestCase):
+    """Test the wait_for_db management command."""
+
+    def test_wait_for_db_ready(self, patched_check):
+        """Command returns immediately if the database is ready."""
+        patched_check.return_value = True
+
+        call_command("wait_for_db")
+
+        patched_check.assert_called_once_with(databases=["default"])
+    
+    @patch("time.sleep")
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Command retries until the database is availbe."""
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
+
+        call_command("wait_for_db")
+
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=["default"])
